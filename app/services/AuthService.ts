846722bfa1a59204a0a93a6c42da5faf816c88ac
Elías Mena <jmenafonseca28@gmail.com>
2025-03-20 02:05:57 -0600

    Rediseño de funcionalidades que utilizaban supabase para no utilizar dependencias deprecated

--- app/services/AuthService.ts
+++ app/services/AuthService.ts
@@ -1,22 +1,13 @@
-//import { supabase } from './SupabaseClientService';
-//import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { supabase } from './ClientServiceSupabase';
 import UserLogin from '@/app/models/UserLogin.model';
 
 export async function login(user: UserLogin) {
     const { email, password } = user;
     const { data, error } = await supabase.auth.signInWithPassword({ email, password });
-    console.log("Info ", data, error);
+
     if (error) {
         console.error('Error al iniciar sesión:', error.message);
         return null;
-    }
-
-    if (data.session) {
-        await supabase.auth.setSession({
-            access_token: data.session.access_token,
-            refresh_token: data.session.refresh_token,
-        });
     }
 
     return data;
