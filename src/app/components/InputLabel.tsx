eb95dc15bed590a912c27590e558f8f61a94db1b
Elías Mena <jmenafonseca28@gmail.com>
2025-04-04 02:22:42 -0600

    Actualizar InputLabel para aceptar propiedades adicionales y mejorar el estilo del input en modo oscuro.

--- src/app/components/InputLabel.tsx
+++ src/app/components/InputLabel.tsx
@@ -7,6 +7,7 @@ export default function InputLabel({
     textLabel,
     typeInput = "text",
     onchange,
+    ...props
 }: InputLabelProps) {
     return (
         <div className="mb-4">
@@ -15,10 +16,11 @@ export default function InputLabel({
             </label>
             <input
                 type={typeInput}
-                className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm dark:text-white"
                 id={inputId}
                 placeholder={inputPlaceHolder}
                 onChange={onchange}
+                {...props}
             />
         </div>
     )
--- src/app/models/ComponentProps/InputLabelProps.ts
+++ src/app/models/ComponentProps/InputLabelProps.ts
@@ -1,6 +1,6 @@
 import { ChangeEvent } from "react";
 
-export interface InputLabelProps {
+export interface InputLabelProps extends React.HTMLProps<HTMLInputElement> {
     inputId: string;
     inputPlaceHolder: string;
     textLabel: string;
