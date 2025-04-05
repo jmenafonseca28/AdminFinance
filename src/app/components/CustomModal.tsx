09e93eb6ce45d872dff21b7863d4e4632985c1f9
Oskillar21 <oscar.calvosiles@ucr.ac.cr>
2025-04-04 21:32:01 -0600

    Cambios en la duracion en el cierre del modal

--- src/app/components/CustomModal.tsx
+++ src/app/components/CustomModal.tsx
@@ -35,7 +35,7 @@ export default function Modal({
         onClickPrimaryButton?.();
         setTimeout(() => {
             dialogRef.current?.close();
-        }, 100);
+        }, 200);
     }
 
     function handleClickSecondaryButton() {
@@ -48,7 +48,7 @@ export default function Modal({
         onClickSecondaryButton?.();
         setTimeout(() => {
             dialogRef.current?.close();
-        }, 100);
+        }, 200);
     }
 
     return (
@@ -56,7 +56,7 @@ export default function Modal({
             ref={dialogRef}
             id={idModal}
             className="border-0 outline-none backdrop:bg-black/50 backdrop:backdrop-blur-sm p-0 shadow-xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-95  rounded-lg bg-gray-800 animate-fade-in"
-            style={{ animationDuration: '0.1s' }}
+            style={{ animationDuration: '0.2s' }}
         >
             <div className="w-full max-w-md">
                 <div className="bg-white dark:bg-gray-800">
