043f40b3edb821584d3969dad826cf79bb20c30d
JALLMAR ANDRES SAMUELS TORRES <jallmar.samuels@ucr.ac.cr>
2025-08-26 14:08:11 -0600

    Ajustes a page404

--- src/app/pages/notFound/page.tsx
+++ src/app/pages/notFound/page.tsx
@@ -37,17 +37,17 @@ export default function NotFoundPage() {
     };
 
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-gray-900 text-white p-6">
-            <h1 className="text-9xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r  from-red-500 to-purple-600 animate-pulse">
+        <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br bg-gray-100 dark:bg-gray-700 p-6">
+            <h1 className="text-9xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r  from-blue-600 to-blue-800 animate-pulse">
                 {count}
             </h1>
 
-            <h2 className="mt-4 text-3xl font-bold">La pagina buscada no ha sido encontrada</h2>
+            <h2 className="mt-4 text-3xl font-bold text-center text-gray-400">La pagina buscada no ha sido encontrada</h2>
             <p className="mt-2 text-gray-400 max-w-md  text-center">
                 Lo sentimos, pero no hemos podido encontrar la página que estabas buscando.
             </p>
 
-            <button onClick={backToHome} className="mt-4 px-4 py-2 bg-red-500 hover:bg-red-600 text-white font-bold rounded">
+            <button onClick={backToHome} className="mt-4 px-4 py-2 bg-blue-800 hover:bg-blue-800 text-white font-bold rounded">
                 Volver a la página de inicio
             </button>
         </div>
